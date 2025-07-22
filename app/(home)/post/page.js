507f1affcde839498f871d7e88ec07edfb544c16
Sky28702 <167354173+Sky28702@users.noreply.github.com>
2025-07-22 19:37:15 +0530

    post page done frontend

--- app/(home)/post/page.js
+++ app/(home)/post/page.js
@@ -50,13 +50,26 @@ function Post() {
         <label className=" block">Add Photos</label>
 
         <label
-          className="flex flex-col justify-center items-center bg-gray-800 md:py-24 cursor-pointer rounded-2xl shadow shadow-blue-800 py-10 "
+          className="flex flex-col justify-center items-center bg-gray-800  cursor-pointer rounded-2xl shadow shadow-blue-800  "
           htmlFor="fileUpload"
         >
-          <IconLibraryPhoto stroke={1} size={120} className=" mb-2" />
+          {selectedImage ? (
+            <img
+              src={
+                selectedImage
+                  ? URL.createObjectURL(selectedImage)
+                  : "/profilePlaceholder.png"
+              }
+              className="object-contain p-0 "
+            ></img>
+          ) : (
+            <div className="md:py-24 py-10">
+              <IconLibraryPhoto stroke={1} size={120} className=" mb-2" />
 
-          <p className="mb-2">Drag Photo here</p>
-          <p className="text-gray-500">PNG, JPG, JPEG</p>
+              <p className="mb-2">Upload Photo</p>
+              <p className="text-gray-500">PNG, JPG, JPEG</p>
+            </div>
+          )}
         </label>
 
         <input
@@ -69,21 +82,21 @@ function Post() {
         <input
           type="text"
           placeholder="Skill, Art, Code"
-          className="p-2 w-full  border-gray-700 shadow shadow-blue-800 rounded bg-gray-800"
+          className="p-2 w-full  border-gray-700 shadow shadow-blue-800 rounded bg-gray-800 mb-10"
           {...register("tags")}
         ></input>
-        <div className="flex flex-row text-left justify-left ">
+        <div className="flex flex-row text-right justify-end mb-4">
           <button
-            type="submit"
-            className="rounded bg-gray-950  px-8 py-2 cursor-pointer"
-          >
-            Post
-          </button>
-          <button
-            className="rounded bg-gray-700 ml-6  px-8 py-2 cursor-pointer"
+            className="rounded bg-gray-800   px-8 py-2 cursor-pointer"
             onClick={handleResetClick}
           >
             Cancel
+          </button>
+          <button
+            type="submit"
+            className="rounded bg-sky-600 ml-6 px-8 py-2 cursor-pointer"
+          >
+            Post
           </button>
         </div>
       </form>

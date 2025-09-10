bb845f27c8ad21627ce651ea38489784cdc8a984
Sky28702 <167354173+Sky28702@users.noreply.github.com>
2025-09-10 23:37:18 +0530

    f

--- components/EditForm.js
+++ components/EditForm.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 import { redirect, RedirectType } from "next/navigation";
 
 function EditForm(props) {
-  const olie = 2336;
+  const olie = 26;
   //   const {
   //     register,
   //     handleSubmit,
